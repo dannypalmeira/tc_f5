637be269aefc55bd5fbcb9bf0737c5bd6f2d779a
JoaoBibito <joao-vitor1903@live.com>
2024-07-22 17:42:27 -0300

    Alocação de tempo

--- client/src/paginas/dashboard.jsx
+++ client/src/paginas/dashboard.jsx
@@ -24,62 +24,102 @@ const TaskTable = () => {
 const Dashboard = () => {
   const {user} = useAuth();
   const [loading, setLoading] = useState(true);
-  let tarefas = {
+  const [tarefas, setTarefas] = useState({
     total: 0,
     completas: 0,
     andamento: 0,
     pendente: 0,
-  };
+  });
+
   useEffect(() => {
     if (user) {
-      setLoading(false);
+      buscaTarefas();
     } else {
       return;
     }
-    const tarefas = async () => {
-      const tarefaData = await buscaTarefasUser(user.id);
-      console.log("tarefaaa", tarefaData.data.length);
-      tarefas.total = tarefaData.data.length;
-    };
-    tarefas();
   }, [user]);
-  const stats = [
-    {
-      _id: "1",
-      label: "TOTAL DE TAREFAS",
-      icon: <FaNewspaper />,
-      bg: "bg-[#1d4ed8]",
-      total: tarefas.total,
-    },
-    {
-      _id: "2",
-      label: "TAREFAS COMPLETADAS",
-      icon: <MdAdminPanelSettings />,
-      bg: "bg-[#0f766e]",
-      total: tarefas.completas,
-    },
-    {
-      _id: "3",
-      label: "TAREFAS EM ANDAMENTO",
-      icon: <LuClipboardEdit />,
-      bg: "bg-[#f59e0b]",
-      total: tarefas.andamento,
-    },
-    {
-      _id: "4",
-      label: "PENDÊNCIAS",
-      icon: <FaArrowsToDot />,
-      bg: "bg-[#be185d]" || 0,
-      total: tarefas.pendente,
-    },
-  ];
-  const Card = ({label, bg, icon, total}) => {
+
+  const buscaTarefas = async () => {
+    const tarefaData = await buscaTarefasUser(user.id);
+    const newTarefas = {
+      total: tarefaData.data.length,
+      totalMinutos: 0,
+      completas: 0,
+      completasMinutos: 0,
+      andamento: 0,
+      andamentoMinutos: 0,
+      pendente: 0,
+      pendenteMinutos: 0,
+    };
+    tarefaData.data.forEach((tarefa) => {
+      let minutos = 0;
+      if (tarefa.prazo === "BAIXA") {
+        minutos = 60;
+      } else if (tarefa.prazo === "MEDIA") {
+        minutos = 30;
+      } else if (tarefa.prazo === "ALTA") {
+        minutos = 10;
+      }
+
+      newTarefas.totalMinutos += minutos;
+      switch (tarefa.situacao) {
+        case "PENDENTE":
+          newTarefas.pendente++;
+          newTarefas.pendenteMinutos += minutos;
+          break;
+        case "EM ANDAMENTO":
+          newTarefas.andamento++;
+          newTarefas.andamentoMinutos += minutos;
+          break;
+        case "FINALIZADA":
+          newTarefas.completas++;
+          newTarefas.completasMinutos += minutos;
+          break;
+        default:
+          break;
+      }
+    });
+    setTarefas(newTarefas);
+    setLoading(false);
+  };
+  // const stats = [
+  //   {
+  //     _id: "1",
+  //     label: "TOTAL DE TAREFAS",
+  //     icon: <FaNewspaper />,
+  //     bg: "bg-[#1d4ed8]",
+  //     total: tarefas.total,
+  //   },
+  //   {
+  //     _id: "2",
+  //     label: "TAREFAS COMPLETADAS",
+  //     icon: <MdAdminPanelSettings />,
+  //     bg: "bg-[#0f766e]",
+  //     total: tarefas.completas,
+  //   },
+  //   {
+  //     _id: "3",
+  //     label: "TAREFAS EM ANDAMENTO",
+  //     icon: <LuClipboardEdit />,
+  //     bg: "bg-[#f59e0b]",
+  //     total: tarefas.andamento,
+  //   },
+  //   {
+  //     _id: "4",
+  //     label: "PENDÊNCIAS",
+  //     icon: <FaArrowsToDot />,
+  //     bg: "bg-[#be185d]" || 0,
+  //     total: tarefas.pendente,
+  //   },
+  // ];
+
+  const Card = ({label, bg, icon, total, tempo}) => {
     return (
       <div className='w-full h-32 bg-white p-5 shadow-md rounded-md flex items-center justify-between'>
         <div className='h-full flex flex-1 flex-col justify-between'>
           <p className='text-base text-gray-600'>{label}</p>
           <span className='text-2xl font-semibold'>{total || 0}</span>
-          <span className='text-sm text-gray-400'>{"50 no ultimo mes"}</span>
+          <span className='text-sm text-gray-400'>{`${tempo} minutos para tarefas`}</span>
         </div>
 
         <div
@@ -98,9 +138,38 @@ const Dashboard = () => {
   ) : (
     <div className='h-full py-4'>
       <div className='grid grid-cols-1 md:grid-cols-4 gap-5'>
-        {stats.map(({icon, bg, label, total}, index) => (
+        <Card
+          label='TOTAL DE TAREFAS'
+          bg='bg-[#1d4ed8]'
+          icon={<FaNewspaper />}
+          total={tarefas.total}
+          tempo={tarefas.totalMinutos}
+        />
+        <Card
+          label='TAREFAS COMPLETAS'
+          bg='bg-[#0f766e]'
+          icon={<MdAdminPanelSettings />}
+          total={tarefas.completas}
+          tempo={tarefas.completasMinutos}
+        />
+        <Card
+          label='TAREFAS EM ANDAMENTO'
+          bg='bg-[#f59e0b]'
+          icon={<LuClipboardEdit />}
+          total={tarefas.andamento}
+          tempo={tarefas.andamentoMinutos}
+        />
+        <Card
+          label='PENDÊNCIAS'
+          bg='bg-[#be185d]'
+          icon={<FaArrowsToDot />}
+          total={tarefas.pendente}
+          tempo={tarefas.pendenteMinutos}
+        />
+
+        {/* {stats.map(({icon, bg, label, total}, index) => (
           <Card key={index} icon={icon} bg={bg} label={label} count={total} />
-        ))}
+        ))} */}
       </div>
 
       <div className='w-full flex flex-col md:flex-row gap-4 2xl:gap-10 py-8'>
--- client/src/services/usuarioService.js
+++ client/src/services/usuarioService.js
@@ -6,24 +6,17 @@ const baseUrl = "http://localhost:4000";
 // Função para buscar os nomes dos usuários de um time específico
 export const buscaUsuariosDoTime = async (idTime) => {
   try {
-
     const response = await axios.get(`${baseUrl}/usuarios`);
     const usuarios = response.data;
 
-
-    console.log("Dados recebidos da API:", usuarios);
-
-
     const nomesUsuariosDoTime = usuarios
-      .filter(usuario =>
-        usuario.data.time && usuario.data.time.includes(idTime)
+      .filter(
+        (usuario) => usuario.data.time && usuario.data.time.includes(idTime)
       )
-      .map(usuario => ({
+      .map((usuario) => ({
         id: usuario.data.uid,
-        nome: usuario.data.nome
+        nome: usuario.data.nome,
       }));
-      
-    console.log("Nomes dos usuários pertencentes ao time:", nomesUsuariosDoTime);
 
     return nomesUsuariosDoTime;
   } catch (error) {
@@ -33,7 +26,6 @@ export const buscaUsuariosDoTime = async (idTime) => {
 };
 
 export const usuarioPertenceATime = async (usuario) => {
-    const response = await axios.get(`${baseUrl}/usuarios/${usuario}`);
-    return response.data.time !== null && response.data.time !== undefined;
-    
-  };
+  const response = await axios.get(`${baseUrl}/usuarios/${usuario}`);
+  return response.data.time !== null && response.data.time !== undefined;
+};
