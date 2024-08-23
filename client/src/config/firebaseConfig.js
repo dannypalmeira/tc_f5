831ebf32cd49a9365223154e6f7818218cc20a9b
Danny Palmeira <99752093+dannypalmeira@users.noreply.github.com>
2024-08-23 15:15:49 +0200

    Update firebaseConfig.js

--- client/src/config/firebaseConfig.js
+++ client/src/config/firebaseConfig.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCODcymVMPeid5Vq5TVM8G4yFEEibtAMdY",
-  authDomain: "postech-fiap-2a4d7.firebaseapp.com",
-  projectId: "postech-fiap-2a4d7",
-  storageBucket: "postech-fiap-2a4d7.appspot.com",
-  messagingSenderId: "164779905537",
-  appId: "1:164779905537:web:d957cc416f87382efb2600"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
